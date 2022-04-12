7cee33d75424b5019ecce4bcad30b5a811c50685
Marga <mmoyafiol@gmail.com>
2022-04-12 18:29:02 +0200

    Añado la opción para el numero neutro

--- signo_numero.py
+++ signo_numero.py
@@ -1,5 +1,7 @@
 def signo_numero(n):
     if n < 0:
         print('Este numero es negativo')
+    elif n == 0:
+        print('Este numero es neutro')
     else:
         print('Este numero es positivo')
